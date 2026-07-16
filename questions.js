42b6c97c6f0a8c116df92ee0168c91efbecde953
Piyal Basak <piyalfreelancing@gmail.com>
2026-07-16 13:25:17 +0600

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,45 @@
+const questions = [
+
+{
+image:"assets/haaland.jpg",
+
+correct:"Erling Haaland",
+
+options:[
+"Erling Haaland",
+"Harry Kane",
+"Kylian Mbappé",
+"Jude Bellingham"
+]
+
+},
+
+{
+image:"assets/mbappe.jpg",
+
+correct:"Kylian Mbappé",
+
+options:[
+"Kylian Mbappé",
+"Vinicius Jr",
+"Erling Haaland",
+"Harry Kane"
+]
+
+},
+
+{
+image:"assets/bellingham.jpg",
+
+correct:"Jude Bellingham",
+
+options:[
+"Jude Bellingham",
+"Pedri",
+"Kevin De Bruyne",
+"Lionel Messi"
+]
+
+}
+
+];
