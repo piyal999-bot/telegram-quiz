e35476961fca824c814aa84c2f71885ad4ec21e2
Piyal Basak <piyalfreelancing@gmail.com>
2026-07-16 13:48:09 +0600

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,12 +1,17 @@
 const questions = [
 
 {
-    zoomImage: "assets/players/haaland_zoom.jpg",
-    fullImage: "assets/players/haaland.jpg",
+    id:1,
+    pack:"classic",
+    mode:"blur",
+    difficulty:"easy",
 
-    correct: "Erling Haaland",
+    image:"assets/blur/haaland.jpg",
+    reveal:"assets/players/haaland.jpg",
 
-    options: [
+    answer:"Erling Haaland",
+
+    options:[
         "Erling Haaland",
         "Harry Kane",
         "Kylian Mbappé",
@@ -15,29 +20,39 @@ const questions = [
 },
 
 {
-    zoomImage: "assets/players/mbappe_zoom.jpg",
-    fullImage: "assets/players/mbappe.jpg",
+    id:2,
+    pack:"classic",
+    mode:"zoom",
+    difficulty:"medium",
 
-    correct: "Kylian Mbappé",
+    image:"assets/zoom/mbappe.jpg",
+    reveal:"assets/players/mbappe.jpg",
 
-    options: [
-        "Kylian Mbappé",
+    answer:"Kylian Mbappé",
+
+    options:[
         "Vinicius Jr",
+        "Kylian Mbappé",
         "Erling Haaland",
         "Harry Kane"
     ]
 },
 
 {
-    zoomImage: "assets/players/bellingham_zoom.jpg",
-    fullImage: "assets/players/bellingham.jpg",
+    id:3,
+    pack:"classic",
+    mode:"pixel",
+    difficulty:"hard",
 
-    correct: "Jude Bellingham",
+    image:"assets/pixel/bellingham.jpg",
+    reveal:"assets/players/bellingham.jpg",
 
-    options: [
-        "Jude Bellingham",
+    answer:"Jude Bellingham",
+
+    options:[
         "Pedri",
         "Kevin De Bruyne",
+        "Jude Bellingham",
         "Lionel Messi"
     ]
 }
